3bbec2d356a253bc7c9b061d6b2395810fd0f0bb
cbauer2018 <ccbauer2018@gmail.com>
2023-12-18 20:13:50 -0700

    Removed same shape check from expand

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -276,7 +276,6 @@ class Tensor:
     new_shape = argfix(shape, *args)
     return mlops.Reshape.apply(self, shape=tuple([-prod(self.shape) // prod(new_shape) if s == -1 else (s if s is not None else self.shape[i]) for i,s in enumerate(new_shape)]))  # noqa: E501
   def expand(self, shape, *args) -> Tensor:
-    if shape == self.shape: return self
     return mlops.Expand.apply(self, shape=tuple([x if x != -1 else s for s,x in zip(self.shape, argfix(shape, *args))]))
   def permute(self, order, *args) -> Tensor: return mlops.Permute.apply(self, order=argfix(order, *args))
   def flip(self, axis, *args) -> Tensor: return mlops.Flip.apply(self, axis=[x if x >= 0 else x+len(self.shape) for x in argfix(axis, *args)])
